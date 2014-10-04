7f97582cf89939422856f1ca6ca488c32f82d424
Marcel Benning <marcel@ccmob.net>
2014-10-04 16:04:30 +0200

    Hurray ! It's working like it should.

--- XMLConfig/src/net/ccmob/xml/XMLConfig.java
+++ XMLConfig/src/net/ccmob/xml/XMLConfig.java
@@ -10,54 +10,86 @@ import java.util.ArrayList;
 
 public class XMLConfig {
 
-	private ArrayList<String> configLines;
+	/**
+	 * Here is the config saved to and can be modified at runtime. After
+	 * modification, call the save method.
+	 */
 	private XMLNode rootNode = new XMLNode("XMLConfig");
 
+	/**
+	 * Stores the fileName if given;
+	 */
+	private String fileName = null;
+
+	/**
+	 * @param filename
+	 *            the file to parse from
+	 */
 	public XMLConfig(String filename) {
 		this(new File(filename));
 	}
 
+	/**
+	 * @param f
+	 *            the file to parse from
+	 */
 	public XMLConfig(File f) {
-		this();
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(f));
-			String line = "";
-			while ((line = reader.readLine()) != null) {
-				this.getConfigLines().add(line);
-			}
-			reader.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		this.setFileName(f.getAbsolutePath());
 		parse();
 	}
 
+	/**
+	 * @param lines
+	 *            - the file content to parse from
+	 */
 	public XMLConfig(ArrayList<String> lines) {
-		this();
-		this.setConfigLines(lines);
-		parse();
+		parseLines(lines);
 	}
 
+	/**
+	 * @param lines
+	 *            the file content to parse from
+	 */
 	public XMLConfig(String[] lines) {
-		this();
+		ArrayList<String> arr = new ArrayList<String>();
 		for (int i = 0; i < lines.length; i++) {
-			this.getConfigLines().add(lines[i]);
+			arr.add(lines[i]);
 		}
-		parse();
+		parseLines(arr);
+		arr.clear();
 	}
 
+	/**
+	 * This function converts the content of the file - specified at the
+	 * constructor - to a XMLNode tree, saved in the rootNode varibale.
+	 */
 	public void parse() {
 		try {
-			this.setConfigNode(XMLParser.parseFileLines(this.getConfigLines()));
+			this.setRootNode(XMLParser.parseFile(this.getFileName()));
 		} catch (EOFException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private XMLConfig() {
-		this.setConfigLines(new ArrayList<String>());
+	/**
+	 * This function converts the content of the ArrayList (the content of a
+	 * file or a string in the first object) to a XMLNode tree, saved in the
+	 * rootNode varibale.
+	 */
+	public void parseLines(ArrayList<String> lines) {
+		try {
+			this.setRootNode(XMLParser.parseFileLines(lines));
+		} catch (EOFException e) {
+			e.printStackTrace();
+		}
 	}
 
+	/**
+	 * This function saves the rootNode to a xmlFile ( @param filename ) with
+	 * proper format
+	 * 
+	 * @param filename
+	 */
 	public void save(String filename) {
 		try {
 			File f = new File(filename);
@@ -65,18 +97,30 @@ public class XMLConfig {
 				f.delete();
 			}
 			FileWriter writer = new FileWriter(f);
-			writeNode(getConfigNode(), 0, writer);
+			writeNode(getRootNode(), 0, writer);
 			writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * formats a XMLAttribute
+	 * 
+	 * @param attr
+	 * @return formated attribute string
+	 */
 	private String formAttribute(XMLAttribute attr) {
 		return attr.getAttributeName() + "=\"" + attr.getAttributeValue()
 				+ "\"";
 	}
 
+	/**
+	 * Creates a string with tabs for saving
+	 * 
+	 * @param tabIndex
+	 * @return
+	 */
 	private String formTabs(int tabIndex) {
 		String tabs = "";
 		for (int i = 0; i < tabIndex; i++) {
@@ -85,6 +129,16 @@ public class XMLConfig {
 		return tabs;
 	}
 
+	/**
+	 * This function writes an XMLNode ( @param node ) to a FileWriter ( @param
+	 * writer ) with the proper format. The @param tabIndex specifies the shift
+	 * to the right in the file for proper reading.
+	 * 
+	 * @param node
+	 * @param tabIndex
+	 * @param writer
+	 * @throws IOException
+	 */
 	private void writeNode(XMLNode node, int tabIndex, FileWriter writer)
 			throws IOException {
 		String line = formTabs(tabIndex) + "<" + node.getName();
@@ -107,45 +161,63 @@ public class XMLConfig {
 	}
 
 	/**
-	 * @return the configLines
+	 * @return the rootNode
 	 */
-	public ArrayList<String> getConfigLines() {
-		return configLines;
-	}
-
-	/**
-	 * @param configLines
-	 *            the configLines to set
-	 */
-	public void setConfigLines(ArrayList<String> configLines) {
-		this.configLines = configLines;
-	}
-
-	/**
-	 * @return the configNode
-	 */
-	public XMLNode getConfigNode() {
+	public XMLNode getRootNode() {
 		return rootNode;
 	}
 
 	/**
-	 * @param configNode
-	 *            the configNode to set
+	 * @param rootNode
+	 *            the rootNode to set
 	 */
-	public void setConfigNode(XMLNode configNode) {
-		this.rootNode = configNode;
+	public void setRootNode(XMLNode rootNode) {
+		this.rootNode = rootNode;
+	}
+
+	/**
+	 * @return the fileName
+	 */
+	public String getFileName() {
+		return fileName;
+	}
+
+	/**
+	 * @param fileName
+	 *            the fileName to set
+	 */
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
 	}
 
 	public static class XMLAttribute {
-
+		/**
+		 * Stores the name of the Attribute
+		 */
 		private String attributeName = "";
+
+		/**
+		 * Sores the value of the Attribute
+		 */
 		private Object attributeValue = "";
 
+		/**
+		 * @param name
+		 *            Attribute name
+		 * @param value
+		 *            Attribute value
+		 */
 		public XMLAttribute(String name, Object value) {
 			this.setAttributeName(name);
 			this.setAttributeValue(value);
 		}
 
+		/**
+		 * Just without the value
+		 * 
+		 * @param name
+		 *            Attribute name
+		 */
 		public XMLAttribute(String name) {
 			this(name, null);
 		}
@@ -183,9 +255,25 @@ public class XMLConfig {
 	}
 
 	public static class XMLNode {
+
+		/**
+		 * Node name
+		 */
 		private String name;
+
+		/**
+		 * Node childs
+		 */
 		private ArrayList<XMLNode> childs;
+
+		/**
+		 * Node attributes
+		 */
 		private ArrayList<XMLAttribute> attributes;
+
+		/**
+		 * The parent of this node
+		 */
 		private XMLNode parent;
 
 		public XMLNode() {
@@ -193,44 +281,95 @@ public class XMLConfig {
 			this.attributes = new ArrayList<XMLAttribute>();
 		}
 
+		/**
+		 * The name of the node:
+		 * 
+		 * @param name
+		 */
 		public XMLNode(String name) {
 			this();
 			this.setName(name);
 		}
-
+		
+		/**
+		 * Adds a attribute to the node
+		 * @param key
+		 * @param value
+		 */
 		public void addAttribute(String key, String value) {
 			this.attributes.add(new XMLAttribute(key, value));
 		}
 
+		/**
+		 * Adds a child to the node
+		 * @param child
+		 */
 		public void addChild(XMLNode child) {
 			this.childs.add(child);
 			child.setParent(this);
 		}
 
+		/**
+		 * Returns a child
+		 * @param index
+		 * @return
+		 */
 		public XMLNode getChild(int index) {
 			return (XMLNode) this.childs.get(index);
 		}
 
+		public XMLNode getChild(String nodeName){
+			for(XMLNode node : this.getChilds()){
+				if(node.getName().equals(nodeName)){
+					return node;
+				}
+			}
+			return null;
+		}
+		
+		/**
+		 * @return The childs array size
+		 */
 		public int getNumChilds() {
 			return this.childs.size();
 		}
 
+		/**
+		 * Set's the parent for this node
+		 * @param parent
+		 */
 		public void setParent(XMLNode parent) {
 			this.parent = parent;
 		}
 
+		/**
+		 * Return the parent of this node
+		 * @return
+		 */
 		public XMLNode getParent() {
 			return this.parent;
 		}
 
+		/**
+		 * Return the name of this node
+		 * @return
+		 */
 		public String getName() {
 			return this.name;
 		}
 
+		/**
+		 * Set's the name of this node
+		 * @param name
+		 */
 		public void setName(String name) {
 			this.name = name;
 		}
 
+		/**
+		 * Returns a formated XMLString
+		 * @return
+		 */
 		public String getXMLString() {
 			return getXMLString(0);
 		}
@@ -243,7 +382,9 @@ public class XMLConfig {
 					+ (getNumChilds() == 0 ? " /" : "") + ">\n";
 			if (this.childs.size() != 0) {
 				for (int i = 0; i < this.childs.size(); i++) {
-					s = s + ((XMLNode) this.childs.get(i)).getXMLString(height + 1);
+					s = s
+							+ ((XMLNode) this.childs.get(i))
+									.getXMLString(height + 1);
 				}
 				for (int j = 0; j < height; j++)
 					s = s + "\t";
@@ -252,6 +393,10 @@ public class XMLConfig {
 			return s;
 		}
 
+		/**
+		 * Creates an String with the attributes of this node
+		 * @return
+		 */
 		private String getAttributeString() {
 			String s = "";
 			for (int i = 0; i < this.attributes.size(); i++) {
@@ -294,10 +439,19 @@ public class XMLConfig {
 			this.attributes = attributes;
 		}
 
+		/**
+		 * Wrapper Method to print an XMLNode
+		 * @param node
+		 */
 		public static void printNode(XMLNode node) {
 			pNode(node, 0);
 		}
 
+		/**
+		 * Prints an XMLNode with tabIndex as right shift amount 
+		 * @param node
+		 * @param tabIndex
+		 */
 		private static void pNode(XMLNode node, int tabIndex) {
 			String tabs = "";
 			for (int i = 0; i < tabIndex; i++) {
@@ -314,68 +468,100 @@ public class XMLConfig {
 			}
 			System.out.println(tabs + "}");
 		}
-		
+
 	}
 
-	public static class XMLParser
-	{
-	  public static XMLNode parseText(String text)
-	    throws EOFException
-	  {
-	    XMLNode currentNode = new XMLNode();
-	    char[] c = text.toCharArray();
-	    for (int i = 0; i < c.length; i++) {
-	      if (c[i] == '/') {
-	        if (currentNode.getParent().getName() == null) return currentNode;
-	        currentNode = currentNode.getParent();
-	      }
-	      if ((c[i] == '<') && (c[(i + 1)] != '/')) {
-	        String tagText = text.substring(i + 1);
-	        XMLNode newNode = new XMLNode();
-	        currentNode.addChild(newNode);
-	        currentNode = newNode;
-	        currentNode.setName(tagText.substring(0, tagText.indexOf(">")).split(" ")[0]);
-	        int tagEnd = min(tagText.indexOf(">"), tagText.indexOf("/"));
+	public static class XMLParser {
+		
+		/**
+		 * @Author PikajuTheBoss
+		 */
+		
+		/**
+		 * Parses a XMLString @param text
+		 * @param text
+		 * @return
+		 * @throws EOFException
+		 */
+		public static XMLNode parseText(String text) throws EOFException {
+			XMLNode currentNode = new XMLNode();
+			char[] c = text.toCharArray();
+			for (int i = 0; i < c.length; i++) {
+				if (c[i] == '/') {
+					if (currentNode.getParent().getName() == null)
+						return currentNode;
+					currentNode = currentNode.getParent();
+				}
+				if ((c[i] == '<') && (c[(i + 1)] != '/')) {
+					String tagText = text.substring(i + 1);
+					XMLNode newNode = new XMLNode();
+					currentNode.addChild(newNode);
+					currentNode = newNode;
+					currentNode.setName(tagText.substring(0,
+							tagText.indexOf(">")).split(" ")[0]);
+					int tagEnd = min(tagText.indexOf(">"), tagText.indexOf("/"));
 
-	        String attribText = tagText.substring(0, tagEnd);
-	        for (String currentAttribute : attribText.split(" ")) {
-	          if (currentAttribute.split("=").length >= 2)
-	            currentNode.addAttribute(currentAttribute.split("=")[0], currentAttribute.split("=")[1].replace("\"", ""));
-	        }
-	        i = i + tagEnd - 1;
-	      }
-	    }
+					String attribText = tagText.substring(0, tagEnd);
+					for (String currentAttribute : attribText.split(" ")) {
+						if (currentAttribute.split("=").length >= 2)
+							currentNode.addAttribute(currentAttribute
+									.split("=")[0],
+									currentAttribute.split("=")[1].replace(
+											"\"", ""));
+					}
+					i = i + tagEnd - 1;
+				}
+			}
 
-	    throw new EOFException("Nodes aren't closed properly");
-	  }
+			throw new EOFException("Nodes aren't closed properly");
+		}
 
-	  public static XMLNode parseFile(String filePath) throws EOFException
-	  {
-	    StringBuilder text = new StringBuilder();
-	    String line = "";
-	    try
-	    {
-	      BufferedReader reader = new BufferedReader(new FileReader(new File(filePath)));
+		/**
+		 * Reads a file and passes it to parseText
+		 * @param filePath
+		 * @return
+		 * @throws EOFException
+		 */
+		public static XMLNode parseFile(String filePath) throws EOFException {
+			StringBuilder text = new StringBuilder();
+			String line = "";
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(
+						new File(filePath)));
 
-	      while ((line = reader.readLine()) != null) text.append(line);
-	      reader.close();
-	    } catch (IOException e) {
-	      e.printStackTrace();
-	    }
-	    return parseText(text.toString());
-	  }
-	  
-	  public static XMLNode parseFileLines(ArrayList<String> lines) throws EOFException {
-		  StringBuilder text = new StringBuilder();
-		  for(int i = 0;i<lines.size();i++){
-			  text.append(lines.get(i));
-		  }
-		  return parseText(text.toString());
-	  }
+				while ((line = reader.readLine()) != null)
+					text.append(line);
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return parseText(text.toString());
+		}
 
-	  private static int min(int a, int b) {
-	    return a < b ? a : b;
-	  }
+		/**
+		 * Passes @param lines as single string to parseText
+		 * @param lines
+		 * @return
+		 * @throws EOFException
+		 */
+		public static XMLNode parseFileLines(ArrayList<String> lines)
+				throws EOFException {
+			StringBuilder text = new StringBuilder();
+			for (int i = 0; i < lines.size(); i++) {
+				text.append(lines.get(i));
+			}
+			return parseText(text.toString());
+		}
+
+		/**
+		 * A minimal function for two integers
+		 * @param a
+		 * @param b
+		 * @return
+		 */
+		private static int min(int a, int b) {
+			return a < b ? a : b;
+		}
 	}
 
 }
